36129d0a4f9986daac3aa4d31beb7c3e33e384c5
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-20 08:45:32 +0000

    [automatic commit] 2025-06-20 08:45:32 UTC ;; communications error to 208.67.222.222#53: timed out 129.127.202.72

--- part2/app.js
+++ part2/app.js
@@ -14,7 +14,7 @@ const pool= mysql.createPool({
     host: process.env.DB_HOST || 'localhost',
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASS || '',
-    
+    database: process.env.DB_NAME || 'DogWalkService',}
 // setup
 app.use(session({
     secret:'secretkey',
