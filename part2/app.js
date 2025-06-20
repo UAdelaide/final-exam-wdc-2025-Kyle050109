9865a4e7482eca8040b8aff2b8c2318efd3482f5
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-20 14:36:19 +0000

    [automatic commit] 2025-06-20 14:36:19 UTC 129.127.202.72

--- part2/app.js
+++ part2/app.js
@@ -123,7 +123,8 @@ app.get('/api/users/me', (req, res) => {
     // if there is logged in user in the session return the user_id and encapsulate as a JSON object
     if (!req.session.user) {
        res.json({user_id: req.session.user.user_id});
-    }else{// otherwise return a 401 Unauthorized error
+    }else{// otherwise return a 401 Unauthorized error to show the user is not logginn
+        
         res.status(401).json({error: 'Unauthorized'});
     }
 });
