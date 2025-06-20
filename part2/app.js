e5d8f1c9c6149505cbb0db09a55219ebd5cba33b
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-20 12:43:36 +0000

    [automatic commit] 2025-06-20 12:43:36 UTC ;; communications error to 208.67.222.222#53: timed out 129.127.202.72

--- part2/app.js
+++ part2/app.js
@@ -88,7 +88,7 @@ app.get ('/walker', (req, res) => {
 app.get ('/logout', (req, res) => {
     req.session.destroy(err => {
         if (err) {
-            console.error('logout error:', err);// if destoty session fails still can redirect to 
+            console.error('logout error:', err);// if destoty session fails still can redirect to homepage
             return res.status(500).send('logout failed');
         }
         res.clearCookie ('connect.sid'); // clear session cookie
